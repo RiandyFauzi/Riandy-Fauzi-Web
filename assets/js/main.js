c9d3a4bec72249dfba5d29f07df26adbca5fc01f
riandyfauzi <riand480@gmail.com>
2021-06-04 19:23:03 +0700

    add particlesjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,17 @@ function scrollActive() {
         }
     })
 }
+
+if (localStorage.getItem('theme') == 'dark')
+    setDarkMode(true)
+
+
+function setDarkMode(isDark) {
+    if (isDark) {
+        document.body.setAttribute('id', 'darkmode')
+        localStorage.setItem('theme', 'dark')
+    } else {
+        document.body.setAttribute('id', '')
+        localStorage.removeItem('theme')
+    }
+}
